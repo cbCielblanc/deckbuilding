1ac13f853407e487e82ec1409a7f4802a9593e90
cbCielblanc <cielblanc.cb@gmail.com>
2025-06-19 22:23:47 +0200

    Set launcher window size

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -7,20 +7,28 @@ class_name MainMenu
 @onready var quit_btn   = $VBox/QuitButton
 
 func _ready() -> void:
-	# Auto-connect des signaux si besoin
-	$VBox/SoloButton.pressed.connect(_on_SoloButton_pressed)
-	$VBox/MultiButton.pressed.connect(_on_MultiButton_pressed)
-	$VBox/TutoButton.pressed.connect(_on_TutoButton_pressed)
-	$VBox/QuitButton.pressed.connect(_on_QuitButton_pressed)
+        # Auto-connect des signaux si besoin
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+        DisplayServer.window_set_size(Vector2i(1280, 720))
+        $VBox/SoloButton.pressed.connect(_on_SoloButton_pressed)
+        $VBox/MultiButton.pressed.connect(_on_MultiButton_pressed)
+        $VBox/TutoButton.pressed.connect(_on_TutoButton_pressed)
+        $VBox/QuitButton.pressed.connect(_on_QuitButton_pressed)
 
 func _on_SoloButton_pressed():
-	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+        DisplayServer.window_set_size(Vector2i(1920, 1080))
+        get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
 func _on_MultiButton_pressed():
-	get_tree().change_scene_to_file("res://scenes/LobbyMenu.tscn")
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+        DisplayServer.window_set_size(Vector2i(1280, 720))
+        get_tree().change_scene_to_file("res://scenes/LobbyMenu.tscn")
 
 func _on_TutoButton_pressed():
-	var main := preload("res://scenes/Main.tscn").instantiate()
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+        DisplayServer.window_set_size(Vector2i(1920, 1080))
+        var main := preload("res://scenes/Main.tscn").instantiate()
 	get_tree().root.add_child(main)
 	var tuto := preload("res://scripts/tutorial_manager.gd").new()
 	main.add_child(tuto)
