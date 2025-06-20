1574182e3032e89e12612a8c8af86f238ac57eea
cbCielblanc <cielblanc.cb@gmail.com>
2025-06-20 21:26:21 +0200

    fix UI visibility and fullscreen

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 class_name GameManager
 
 @export var biomes      : PackedStringArray = ["Forest", "Volcano"]
--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -19,9 +19,9 @@ func _ready() -> void:
 	$VBox/QuitButton.pressed.connect(_on_QuitButton_pressed)
 
 func _on_SoloButton_pressed() -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-	DisplayServer.window_set_size(Vector2i(1920, 1080))
-	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+        DisplayServer.window_set_size(Vector2i(1920, 1080))
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+        get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
 func _on_MultiButton_pressed() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
@@ -29,8 +29,8 @@ func _on_MultiButton_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/LobbyMenu.tscn")
 
 func _on_TutoButton_pressed() -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-	DisplayServer.window_set_size(Vector2i(1920, 1080))
+        DisplayServer.window_set_size(Vector2i(1920, 1080))
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 	var main := preload("res://scenes/Main.tscn").instantiate()
 	get_tree().root.add_child(main)
 	var tuto := preload("res://scripts/tutorial_manager.gd").new()
