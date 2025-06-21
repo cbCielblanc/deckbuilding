2ea75dc9930d460c58b7064e401a871a19d298a0
Coraillon <valentin.dauzere-peres@optimal-logistics.no>
2025-06-21 13:34:01 +0200

    k

--- ui/stats_ui.gd
+++ ui/stats_ui.gd
@@ -6,18 +6,18 @@ var player : Player
 @onready var end_button : Button = $EndTurn
 
 func _ready() -> void:
-        player = get_node(player_path)
-        player.add_to_group("local_player")
-        player.connect("stats_changed", Callable(self, "_refresh"))
-        end_button.pressed.connect(_on_end_pressed)
-        _refresh()
+	player = get_node(player_path)
+	player.add_to_group("local_player")
+	player.connect("stats_changed", Callable(self, "_refresh"))
+	end_button.pressed.connect(_on_end_pressed)
+	_refresh()
 
 func _refresh(_p : Player = null) -> void:
 	$Life.text = "Life: %d" % player.life
 	$Gold.text = "Gold: %d" % player.gold
 	$Essence.text = "Essence: %d" % player.essence
-        $Mana.text = "Mana: %d" % player.mana
+	$Mana.text = "Mana: %d" % player.mana
 
 func _on_end_pressed() -> void:
-        player.end_turn()
-        EventBus.emit("event", {"tag":"turn_end"})
+	player.end_turn()
+	EventBus.emit("event", {"tag":"turn_end"})
